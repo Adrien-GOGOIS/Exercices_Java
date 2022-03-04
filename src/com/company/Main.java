757e05b86eb6870bdaf6492736808380689ea908
Adrien-Gogois <ad.gogois@gmail.com>
2022-03-04 15:28:35 +0100

    Exercice sur les tableaux

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,12 +21,27 @@ public class Main {
         float number = 23.58f;
         System.out.println(Math.round(number));
 
-        // LENGTH
+        // LENGTH & TABLEAU
 
-        int[] array = new int[5];
+        long[] array = {10L, 5L, 8L};
+        int[] array2 = new int[10];
         String phrase = "Ceci est une phrase";
         System.out.println("La taille du tableau est de " + array.length);
+        System.out.println("La taille du tableau est de " + array2.length);
         System.out.println("La longueur de la phrase est de " + phrase.length());
+
+        // EXERCICES SUR LES TABLEAUX
+
+        int[] array3 = new int[5];
+        array3[0] = 2;
+        array3[1] = 8;
+        array3[2] = 10;
+        array3[3] = 7;
+        array3[4] = 2;
+
+        for (int i = 0; i < array3.length; i++) {
+            System.out.println("Element " + i + ": " + array3[i]);
+        }
 
     }
 }
